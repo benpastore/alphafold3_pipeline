5cb69ba60e2f3bff8a1c4033dfacab83e820b2a8
benpastore <ben.pastore14@gmail.com>
2025-03-27 13:30:31 -0400

    add cluster directive to slurm

--- run.sh
+++ run.sh
@@ -39,7 +39,7 @@ echo """#!/bin/bash
 
 #SBATCH --no-requeue
 #SBATCH -c 1
-#SBATCH --account=PCON0160 --ntasks-per-node=1 --time=05:00:00 --mem=2gb --partition=nextgen
+#SBATCH --account=PCON0160 --ntasks-per-node=1 --time=05:00:00 --mem=2gb --partition=nextgen --cluster=ascend-nextgen
 
 nextflow $SCRIPT_DIR/main.nf $@
 
